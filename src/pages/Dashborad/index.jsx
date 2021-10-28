7d9d3ca005917ada92f82071780fdd16cfb850b1
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-28 20:49:29 -0300

    adicionando aside responsivo e funcao de toggle

--- src/pages/Dashborad/index.jsx
+++ src/pages/Dashborad/index.jsx
@@ -3,14 +3,23 @@ import { Dashboard, Sidebar } from "./styles";
 import { useAuth } from "../../hooks/useAuth";
 import { Button } from "../../components/Button";
 import Header from "../../components/Header";
+import { MdMenu } from "react-icons/md";
+import { useState } from "react";
 
 export default function Index() {
     const { user, logout } = useAuth();
 
+    // states
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    function toggleOpenSidebar() {
+        setSidebarOpen((prev) => !prev);
+    }
+
     return (
         <Dashboard>
             <Header user={user} />
-            <Sidebar>
+            <Sidebar active={sidebarOpen}>
                 <p>{moment().format("Do-MMM")}</p>
                 <div>
                     <nav>
@@ -19,7 +28,16 @@ export default function Index() {
                     <Button onClick={logout}>Button</Button>
                 </div>
             </Sidebar>
-            <main>to be render</main>
+
+            <main>
+                <button
+                    className="toggle"
+                    onClick={toggleOpenSidebar}
+                    teste={sidebarOpen}
+                >
+                    <MdMenu />
+                </button>
+            </main>
         </Dashboard>
     );
 }
--- src/pages/Dashborad/styles.jsx
+++ src/pages/Dashborad/styles.jsx
@@ -13,6 +13,15 @@ export const Dashboard = styled.div`
     padding: 1rem;
     gap: 1rem;
 
+    @media (max-width: 600px) {
+        grid-template-columns: 1fr;
+        grid-template-rows: max-content 1fr;
+        grid-template-areas:
+            "header"
+            "main"
+            "main";
+    }
+
     > * {
         padding: 1rem;
         border-radius: 6px;
@@ -31,14 +40,44 @@ export const Dashboard = styled.div`
     main {
         grid-area: main;
     }
+
+    .toggle {
+        background-color: ${(props) => props.theme.green};
+        color: ${(props) => props.theme.light};
+        display: grid;
+        place-items: center;
+        padding: 0.5rem;
+        border: none;
+        outline: none;
+        border-radius: 50%;
+        font-size: 1.25rem;
+        position: relative;
+
+        transform: ${(props) =>
+            props.teste ? "translateX(163px)" : "translateX(0)"};
+
+        @media (min-width: 600px) {
+            display: none;
+        }
+    }
 `;
 
 export const Sidebar = styled.aside`
     display: flex;
     flex-direction: column;
 
-    transition: transform 400ms ease;
-    transform: translateX(0);
+    @media (max-width: 600px) {
+        position: fixed;
+        left: 0;
+        top: calc(64px + 2rem);
+        bottom: 1rem;
+
+        transition: transform 400ms ease;
+        transform: ${(props) =>
+            props.active ? "translateX(1rem)" : "translateX(-100%)"};
+
+        padding-right: 4rem;
+    }
 
     > p {
         font-size: 1.125rem;
